042813a6e28344455525b513ee9718212faff919
AxewB <speedwow1@gmail.com>
2024-07-17 20:05:37 +0300

    product related components

---
+++ frontend/src/components/product/ProductList.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-sheet>
+    
+  </v-sheet>
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue';
+import { useProductStore } from '@/stores/productStore';
+
+const productStore = useProductStore();
+
+onMounted(() => {
+  productStore.getProducts();
+})
+</script>
