aae022c04e57b2b6d4a2bd93efc7514fbe00212c
AxewB <speedwow1@gmail.com>
2024-07-17 20:06:41 +0300

    added repo annotation

--- src/main/java/ru/aksenov/onlineshop/repository/CategoryRepository.java
+++ src/main/java/ru/aksenov/onlineshop/repository/CategoryRepository.java
@@ -1,7 +1,9 @@
 package ru.aksenov.onlineshop.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.aksenov.onlineshop.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
