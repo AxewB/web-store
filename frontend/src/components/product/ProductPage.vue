96cc8fa42d48350b678660fd0b34795e632b913d
AxewB <speedwow1@gmail.com>
2024-07-18 17:00:20 +0300

    beginning of product page template

--- frontend/src/components/product/ProductPage.vue
+++ frontend/src/components/product/ProductPage.vue
@@ -1,8 +1,28 @@
 <template>
-  <v-sheet>
-asd
-{{ product }}
-<!-- {{ productStore.product }} -->
+  <v-sheet class="d-flex flex-column">
+    <AppHeader/>
+    <v-container width="100%" class="bg-red">
+    <v-row no-gutters>
+      <v-col sm="4">
+        <v-sheet class="ma-2 pa-2">
+          One of three columns
+        </v-sheet>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col  sm="4">
+        <v-sheet class="ma-2 pa-2">
+          One of three columns
+        </v-sheet>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col width="100%" class="bg-red" sm="4">
+        <v-sheet class="ma-2 pa-2">
+          One of three columns
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+    {{ product }}
   </v-sheet>
 </template>
 
@@ -10,13 +30,11 @@ asd
 import { onMounted, ref } from 'vue'
 import {useProductStore} from '@/stores/productStore'
 import { useRoute } from 'vue-router';
+import AppHeader from "@/components/AppHeader.vue";
 
 const route = useRoute()
 const product = ref({})
 const productStore = useProductStore();
-// const product = computed(() => {
-//   return productStore.product;
-// })
 onMounted(async () => {
   const id = route.params.id
   product.value = await productStore.getProduct(id)
