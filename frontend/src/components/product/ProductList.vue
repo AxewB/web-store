beb68bfb5acb8f4795e856b801712585bdc58abd
AxewB <speedwow1@gmail.com>
2024-07-19 19:07:14 +0300

    removed commented code

--- frontend/src/components/product/ProductList.vue
+++ frontend/src/components/product/ProductList.vue
@@ -15,38 +15,6 @@ const products = computed(() => {
 })
 
 onMounted(async() => {
-  // let products = '';
-  // for (let i = 0; i < 40; i++) {
-  //   const {id, price, description, title, stock, images, thumbnail} = dummyProducts['products'][i]
-  //   let images_str = ""
-  //   for (let i = 0; i < images.length; i++) {
-  //     images_str += `\"${images[i]}\"${ i < images.length - 1 ? ',' : ''}`
-  //   }
-  //   images_str += ""
-  //   products += `(${id}, ${price}, "${description}", "${title}", ${stock}, '{${images_str}}', "${thumbnail}"),\n`
-  // }
-  // console.log(products)
-
-
-
-
-
-  // console.log(JSON.stringify(dummyProducts['products']))
-  // let images = `'`
-  // let requests = ''
-  // // images += `'`;
-  // // console.log(`(${product.id}, ${product.price}, ${product.description}, ${product.title}, ${product.stock}, ${images}, ${product.thumbnail})`)
-  // for (const product in dummyProducts) {
-    
-  //   for (let i = 0; i < product.images.length; i++) {
-  //     console.log(product.images[i])
-  //     images += `{${product.images[i]}}${i < product.images.length - 1 ? ',' : ''}`
-  //   }
-  //   requests += `(${product.id}, ${product.price}, ${product.description}, ${product.title}, ${product.stock}, ${product.images}, ${product.thumbnail})\n`
-
-  // }
-  // console.log(requests)
-  
   productStore.getProducts();
 })
 </script>
